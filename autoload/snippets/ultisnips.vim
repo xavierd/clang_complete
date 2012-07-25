xavierd/clang_complete

commit 5047c524ca819c6172f988184b5ce45f06ddc6a2

Fix integration with UltiSnips
Signed-off-by: smancill <smancill.m@gmail.com>

autoload/snippets/ultisnips.vim
@@ -2,7 +2,7 @@
 " Author: Philippe Vaucher
 
 function! snippets#ultisnips#init()
-  UltiSnipsAddFiletypes cpp.clang_complete
+  UltiSnipsAddFiletypes &filetype.clang_complete
   call snippets#ultisnips#reset()
 endfunction
 

commit 06efd737e0568bb7ce6d617f60530302b4973f58

Another fix to integration with UltiSnips
This fixes the command :UltiSnipsEdit for C/C++ files, which was broken
by commit 5047c52.

Signed-off-by: smancill <smancill.m@gmail.com>

autoload/snippets/ultisnips.vim
@@ -2,7 +2,7 @@
 " Author: Philippe Vaucher
 
 function! snippets#ultisnips#init()
-  UltiSnipsAddFiletypes &filetype.clang_complete
+  exe "UltiSnipsAddFiletypes ".&filetype.".clang_complete"
   call snippets#ultisnips#reset()
 endfunction
 