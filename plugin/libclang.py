5c1506ac761a76e13c2e944089113401f88919c4
Tobias Grosser <tobias@grosser.es>
2013-01-12 16:17:42 +0100

    Automatically use libclang if available
    If the user did not request anything specific, we first try to use
    libclang and only fall back to the clang executable if libclang could
    not be found. Error messages are only printed in debugging mode or
    if the user explicitly requested libclang.
    
    This patch allows us to use libclang.so as soon as it can be found in
    the system library path. No further configuration is required in
    .vimrc.

--- plugin/libclang.py
+++ plugin/libclang.py
@@ -36,8 +36,12 @@ def getBuiltinHeaderPath(library_path):
     return path
   return None
 
-def initClangComplete(clang_complete_flags, library_path):
+def initClangComplete(clang_complete_flags, library_path, user_requested):
   global index
+
+  debug = int(vim.eval("g:clang_debug")) == 1
+  printWarnings = (user_requested != "0") or debug
+
   if library_path != "":
     Config.set_library_path(library_path)
 
@@ -46,11 +50,12 @@ def initClangComplete(clang_complete_flags, library_path):
   try:
     index = Index.create()
   except Exception, e:
-    print "Loading libclang failed, falling back to clang executable. ",
-    if library_path == "":
-      print "Consider setting g:clang_library_path"
-    else:
-      print "Are you sure '%s' contains libclang?" % library_path
+    if printWarnings:
+      print "Loading libclang failed, falling back to clang executable. ",
+      if library_path == "":
+        print "Consider setting g:clang_library_path"
+      else:
+        print "Are you sure '%s' contains libclang?" % library_path
     return 0
 
   global builtinHeaderPath
@@ -58,7 +63,7 @@ def initClangComplete(clang_complete_flags, library_path):
   if not canFindBuiltinHeaders(index):
     builtinHeaderPath = getBuiltinHeaderPath(library_path)
 
-    if not builtinHeaderPath:
+    if not builtinHeaderPath and printWarnings:
       print "WARNING: libclang can not find the builtin includes."
       print "         This will cause slow code completion."
       print "         Please report the problem."
