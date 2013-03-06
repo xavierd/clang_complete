d96a2bf7b9acefe18a90786c5cdb3bac4a183d1a
Xavier Deguillard <deguilx@gmail.com>
2013-03-05 22:30:29 -0800

    Let's try with ${} markers for clang_complete native snippets.
    Those markers are smaller and less disturbing than <# #>

--- plugin/snippets/clang_complete.py
+++ plugin/snippets/clang_complete.py
@@ -5,19 +5,20 @@ def snippetsInit():
   vim.command("noremap <silent> <buffer> <tab> :python updateSnips()<CR>")
   vim.command("snoremap <silent> <buffer> <tab> <ESC>:python updateSnips()<CR>")
   if int(vim.eval("g:clang_conceal_snippets")) == 1:
-    vim.command("syntax match Conceal /<#/ conceal")
-    vim.command("syntax match Conceal /#>/ conceal")
+    vim.command("syntax match placeHolder /\${.*}/ contains=placeHolderMark")
+    vim.command("syntax match placeHolderMark contained /\${/ conceal")
+    vim.command("syntax match placeHolderMark contained /}/ conceal")
 
 # The two following function are performance sensitive, do _nothing_
 # more that the strict necessary.
 
 def snippetsFormatPlaceHolder(word):
-  return "<#%s#>" % word
+  return "${%s}" % word
 
 def snippetsAddSnippet(fullname, word):
   return word
 
-r = re.compile('<#[^#]*#>')
+r = re.compile('\${[^}]*}')
 
 def snippetsTrigger():
   if r.search(vim.current.line) is None:
@@ -41,6 +42,6 @@ def updateSnips():
   start, end = result.span()
   vim.current.window.cursor = row, start
   isInclusive = vim.eval("&selection") == "inclusive"
-  vim.command('call feedkeys("v%dl\<C-G>", "n")' % (end - start - isInclusive))
+  vim.command('call feedkeys("\<ESC>v%dl\<C-G>", "n")' % (end - start - isInclusive))
 
 # vim: set ts=2 sts=2 sw=2 expandtab :
