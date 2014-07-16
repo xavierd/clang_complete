xavierd/clang_complete

commit 6f491682af87848d92446a68279ecfbb63d81f07

Allow jumping to definition in the preview window

plugin/libclang.py
@@ -504,20 +504,24 @@ def getAbbr(strings):
       return chunks.spelling
   return ""
 
-def jumpToLocation(filename, line, column):
-  if filename != vim.current.buffer.name:
-    try:
-      vim.command("edit %s" % filename)
-    except:
-      # For some unknown reason, whenever an exception occurs in
-      # vim.command, vim goes crazy and output tons of useless python
-      # errors, catch those.
-      return
+def jumpToLocation(filename, line, column, preview):
+  if preview:
+    command = "pedit +%d %s" % (line, filename)
+  elif filename != vim.current.buffer.name:
+    command = "edit %s" % filename
   else:
-    vim.command("normal m'")
-  vim.current.window.cursor = (line, column - 1)
+    command = "normal m"
+  try:
+    vim.command(command)
+  except:
+    # For some unknown reason, whenever an exception occurs in
+    # vim.command, vim goes crazy and output tons of useless python
+    # errors, catch those.
+    return
+  if not preview:
+    vim.current.window.cursor = (line, column - 1)
 
-def gotoDeclaration():
+def gotoDeclaration(preview=True):
   global debug
   debug = int(vim.eval("g:clang_debug")) == 1
   params = getCompileParams(vim.current.buffer.name)
@@ -542,8 +546,8 @@ def gotoDeclaration():
         if d is not None and loc != d.location:
           loc = d.location
           if loc.file is not None:
-            jumpToLocation(loc.file.name, loc.line, loc.column)
-            break
+            jumpToLocation(loc.file.name, loc.line, loc.column, preview)
+          break
 
   timer.finish()
 

plugin/clang_complete.vim
@@ -113,6 +113,10 @@ function! s:ClangCompleteInit()
     let g:clang_jumpto_declaration_key = '<C-]>'
   endif
 
+  if !exists('g:clang_jumpto_declaration_in_preview_key')
+    let g:clang_jumpto_declaration_in_preview_key = '<C-W>]'
+  endif
+
   if !exists('g:clang_jumpto_back_key')
     let g:clang_jumpto_back_key = '<C-T>'
   endif
@@ -159,7 +163,8 @@ function! s:ClangCompleteInit()
     inoremap <expr> <buffer> . <SID>CompleteDot()
     inoremap <expr> <buffer> > <SID>CompleteArrow()
     inoremap <expr> <buffer> : <SID>CompleteColon()
-    execute "nnoremap <buffer> <silent> " . g:clang_jumpto_declaration_key . " :call <SID>GotoDeclaration()<CR><Esc>"
+    execute "nnoremap <buffer> <silent> " . g:clang_jumpto_declaration_key . " :call <SID>GotoDeclaration(0)<CR><Esc>"
+    execute "nnoremap <buffer> <silent> " . g:clang_jumpto_declaration_in_preview_key . " :call <SID>GotoDeclaration(1)<CR><Esc>"
     execute "nnoremap <buffer> <silent> " . g:clang_jumpto_back_key . " <C-O>"
   endif
 
@@ -481,9 +486,9 @@ function! s:CompleteColon()
   return ':' . s:LaunchCompletion()
 endfunction
 
-function! s:GotoDeclaration()
+function! s:GotoDeclaration(preview)
   try
-    python gotoDeclaration()
+    python gotoDeclaration(vim.eval('a:preview') == '1')
   catch /^Vim\%((\a\+)\)\=:E37/
     echoe "The current file is not saved, and 'hidden' is not set."
           \ "Either save the file or add 'set hidden' in your vimrc."

commit 4b53210472b4a53819be5a9114d967eb18e0db5d

set mark when jumping to location in current file
The normal mode command m' was there before but got broken by commit 6f491682: m without the single quote is not a normal mode command. When changing files, the jump list is already updated.

plugin/libclang.py
@@ -503,7 +503,7 @@ def jumpToLocation(filename, line, column, preview):
   elif filename != vim.current.buffer.name:
     command = "edit %s" % filenameEscaped
   else:
-    command = "normal m"
+    command = "normal m'"
   try:
     vim.command(command)
   except: