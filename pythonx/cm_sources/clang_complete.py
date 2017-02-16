69e29baa50d72bca60ceb266a485e849eb573be3
roxma <l>
2017-02-16 21:32:10 +0800

    as nvim-completion-manager source.

---
+++ pythonx/cm_sources/clang_complete.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+
+# For debugging
+# NVIM_PYTHON_LOG_FILE=nvim.log NVIM_PYTHON_LOG_LEVEL=INFO nvim
+
+# clang python bindings only supports python2
+
+import cm
+cm.register_source(name='clang_complete',
+                   priority=9,
+                   abbreviation='c',
+                   scopes=['c','cpp'],
+                   events=['BufEnter'],
+                   detach=1,
+                   python='python2',
+                   cm_refresh_patterns=[r'[0-9a-zA-Z_#]{3,}$',r'(-\>|\.|::)[\w_]*$'],
+                   )
+
+import sys
+import os
+import re
+import logging
+import jedi
+import io
+from neovim import attach, setup_logging
+
+logger = logging.getLogger(__name__)
+
+class Source:
+
+    def __init__(self,nvim):
+
+        self._nvim = nvim
+
+        libclang_base = nvim.eval("globpath(&rtp,'plugin/clang',1)").split("\n")[0]
+        libclang_base = os.path.dirname(libclang_base)
+        logger.info("libclang_base: %s", libclang_base)
+        sys.path.append(libclang_base)
+        # hack, libclang has 'import vim'
+        sys.modules['vim'] = nvim
+
+        import libclang
+
+        # TODO: clean these hacks
+        import snippets.dummy as clang_complete_snippets
+        libclang.snippetsInit = clang_complete_snippets.snippetsInit
+        libclang.snippetsFormatPlaceHolder = clang_complete_snippets.snippetsFormatPlaceHolder
+        libclang.snippetsAddSnippet = clang_complete_snippets.snippetsAddSnippet
+        libclang.snippetsTrigger = clang_complete_snippets.snippetsTrigger
+        libclang.snippetsReset = clang_complete_snippets.snippetsReset
+
+        self._libclang = libclang
+
+        libclang.initClangComplete(nvim.eval('g:clang_complete_lib_flags'),
+                                   nvim.eval('g:clang_compilation_database'),
+                                   nvim.eval('g:clang_library_path'),
+                                   )
+        
+    def cm_refresh(self,info,ctx,*args):
+
+        lnum = ctx['lnum']
+        col = ctx['col']
+        typed = ctx['typed']
+        path = ctx['filepath']
+        
+        debug = False
+
+        kwtyped = re.search(r'[0-9a-zA-Z_]*?$',typed).group(0)
+        startcol = col-len(kwtyped)
+
+        src = cm.get_src(ctx)
+        if not src.strip():
+            return
+
+        file = (path, str(src))
+
+        params = self._libclang.getCompileParams(path)
+        timer = self._libclang.CodeCompleteTimer(debug, path, lnum, startcol, params)
+        t = self._libclang.CompleteThread(lnum, startcol, file, path, params, timer)
+        t.run()
+        cr = t.result
+
+        cr = t.result
+        if cr is None:
+            logger.error("Cannot parse this source file. The following arguments are used for clang: %s", params['args'])
+            return (str([]), timer)
+
+        results = cr.results
+
+        base = typed[startcol-1:]
+        if base != "":
+            results = [x for x in results if self._libclang.getAbbr(x.string).startswith(base)]
+
+        sorting = self._nvim.eval("g:clang_sort_algo")
+        if sorting == 'priority':
+            getPriority = lambda x: x.string.priority
+            results = sorted(results, key=getPriority)
+        if sorting == 'alpha':
+            getAbbrevation = lambda x: self._libclang.getAbbr(x.string).lower()
+            results = sorted(results, key=getAbbrevation)
+
+        matches = list(map(self._libclang.formatResult, results))
+
+        # logger.info("src: %s", src)
+        logger.info("completion result: %s", matches)
+
+        self._nvim.call('cm#complete', info['name'], ctx, startcol, matches, True, async=True)
+
+
